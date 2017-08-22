24255c2fc2bff99f20da42fdac5020a98e5d7020
hongguangjie <guangjie@meizu.com>
2017-08-22 11:41:45 +0800

    完成基本工程化

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ var del = require('del');
 var rename = require('gulp-rename');
 var md5 = require("gulp-md5-plus");
 var fs = require('fs');
+var htmlmin = require('gulp-htmlmin');
+var uglifycss = require('gulp-uglifycss');
+var uglify = require('gulp-uglify');
+
+var isProd = process.env.NODE_ENV === 'production';
 
 var outPath = {
   clear: './export',
@@ -20,9 +25,10 @@ var outPath = {
 
 var srcPath = {
   scss: './src/scss/**/*.scss',
-  js: './src/js/*.js',
+  css: './src/css/**/*.css',
+  js: './src/js/**/*.js',
   swigFull: './src/views/**/*.html',
-  swig: ['./src/views/**/*.html', '!./src/views/_*.html'],
+  swig: ['./src/views/**/*.html', '!./src/views/**/_*.html'],
   images: ['./src/images/**/*'],
   files: './src/files/**/*'
 };
@@ -31,39 +37,48 @@ var browsers = ['> 1%', 'ie >= 9', 'last 2 versions', 'Android >= 4.4'];
 
 gulp.task('scss', function() {
   return gulp.src(srcPath.scss)
-              .pipe(sass({ compress: true }))
-              .pipe(autoprefixer({browsers: browsers}))
-              .pipe(md5(8, outPath.html + '/*.html'))
-              .pipe(gulp.dest(outPath.css));
+    .pipe(sass({
+      compress: isProd
+    }))
+    .pipe(autoprefixer({
+      browsers: browsers
+    }))
+    .pipe(md5(8, outPath.html + '/*.html'))
+    .pipe(gulp.dest(outPath.css));
 });
 
-gulp.task('scss:watch', function() {
-  return gulp.watch(srcPath.scss, ['scss']);
+gulp.task('css', function() {
+  var g = gulp.src(srcPath.css)
+    .pipe(md5(8, outPath.html + '/*.html'));
+  if (isProd) {
+    g = g.pipe(uglifycss());
+  }
+  return g.pipe(gulp.dest(outPath.css));
 });
 
 gulp.task('swig', function() {
-  return gulp.src(srcPath.swig)
-             .pipe(swig({
-                defaults: {
-                 cache: false,
-                 locals: {}
-               }
-             }))
-             .pipe(gulp.dest(outPath.html));
-});
-
-gulp.task('swig:watch', function() {
-  return gulp.watch(srcPath.swigFull, ['swig']);
-});
-
-gulp.task('images', function() {
-  return gulp.src(srcPath.images)
-             .pipe(gulp.dest(outPath.images));
+  var g = gulp.src(srcPath.swig)
+    .pipe(swig({
+      defaults: {
+        cache: false,
+        locals: {
+          isProd: true,
+          version: 1
+        }
+      }
+    }));
+  if (isProd) {
+    g = g.pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyJS: true
+    }));
+  }
+  return g.pipe(gulp.dest(outPath.html));
 });
 
 gulp.task('files', function() {
   return gulp.src(srcPath.files)
-             .pipe(gulp.dest(outPath.files));
+    .pipe(gulp.dest(outPath.files));
 });
 
 gulp.task('del:dirPath', function() {
@@ -73,34 +88,37 @@ gulp.task('del:dirPath', function() {
 });
 
 gulp.task('hash:css', ['swig'], function() {
-  gulp.start(['scss']);
+  gulp.start(['scss', 'css']);
 });
 
 gulp.task('hash:js', function() {
-  return gulp.src(outPath.js + '/*.js')
-             .pipe(md5(8, outPath.html + '/*.html'))
-             .pipe(gulp.dest(outPath.js));
+  var g = gulp.src(srcPath.js)
+    .pipe(md5(8, outPath.html + '/*.html'));
+  if (isProd) {
+    g = g.pipe(uglify());
+  }
+  return g.pipe(gulp.dest(outPath.js));
 });
 
 gulp.task('hash:img', function() {
-  var g = gulp.src(outPath.images + '/**/*')
-              .pipe(md5(8, outPath.html + '/*.html'))
-              .pipe(md5(8, outPath.css + '/*.css'))
-              .pipe(gulp.dest(outPath.images));
+  var g = gulp.src(srcPath.images)
+    .pipe(md5(8, outPath.html + '/*.html'))
+    .pipe(md5(8, outPath.css + '/*.css'))
+    .pipe(gulp.dest(outPath.images));
 });
 
 //1
-gulp.task('swig', ['del:dirPath'], function() {
+gulp.task('swig_task', ['del:dirPath'], function() {
   gulp.start(['swig']);
 });
 
 //2
-gulp.task('css', ['hash:css', 'images', 'files']);
+gulp.task('css_task', ['hash:css', 'files']);
 
 //3、webpack
 
 //4
-gulp.task('js', ['hash:js']);
+gulp.task('js_task', ['hash:js']);
 
 //5
-gulp.task('images', ['hash:img']);
+gulp.task('images_task', ['hash:img']);
