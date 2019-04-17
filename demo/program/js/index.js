4b98df37ab125142f88a2a1538a452dee05c8bf1
shellhong6 <shellhong6@gmail.com>
2019-04-17 17:35:30 +0800

    update

--- demo/program/js/index.js
+++ demo/program/js/index.js
@@ -544,6 +544,43 @@ var gen = aysncFn()
 gen.next()
 
 /**
+ * 实现async/await中的async函数
+ */
+
+function spawn(genF) {
+  return new Promise(function(resolve, reject) {
+    const gen = genF()
+
+    function step (nextF) {
+      let next
+      try {
+        next = nextF()
+      } catch (e) {
+        return reject(e)
+      }
+      if (next.done) {
+        return resolve(next.value)
+      }
+      Promise.resolve(next.value).then(
+        function (v) {
+          step(function () {
+            return gen.next(v)
+          })
+        },
+        function (e) {
+          step(function () {
+            return gen.throw(e)
+          })
+        }
+      )
+    }
+    step(function() {
+      return gen.next(undefined)
+    })
+  })
+}
+
+/**
  * Event Loop
  * http://www.cnblogs.com/jiasm/p/9482443.html
  */
